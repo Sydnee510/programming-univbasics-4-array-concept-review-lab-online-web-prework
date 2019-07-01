77c3fa6055572fcedffc4adf29982a46c9a89f45
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-07-01 22:26:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,7 @@ end
 
 def find_max_value(array)
  array = [1,2,1,3,4,3,5,4,3,2,1])
-  array.max_value(highest value_to_find integer)
+  array.max_value(value_to_find)
 end
 
 def find_min_value(array)
